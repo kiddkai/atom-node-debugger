1198c391cd61dbd0a613ae9c485121742d49d8aa
kiddkai <kiddkai@gmail.com>
2014-10-31 03:20:27 +0800

    [doc] update example

--- demo/a.js
+++ demo/a.js
@@ -1,8 +1,13 @@
 var b = require('./b')
   , result
 
+
+var c = 100;
+
 console.log('this is a log')
 console.error('this is an error')
+
+var d = 10;
 
 b(100, function(err, res) {
   result = res
